9c13a2275cca678ba41dbe3e1c2bc7044d1bd8ce
giuliana <giuliana.torres05@gmail.com>
2023-03-22 22:49:42 -0600

    Created functions to take the text from a url or path and send it to MarkovMachine class.

--- makeText.js
+++ makeText.js
@@ -1,2 +1,44 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+const markov = require("./markov");
+
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text)
+    console.log(mm.makeText())
+}
+
+function fileText(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        }
+        generateText(data);
+    })
+}
+
+async function urlText(url) {
+    try {
+        let res = await axios.get(url);
+        generateText(res.data)
+    } catch (err) {
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1)
+    }
+
+}
+
+let text = process.argv[2];
+let path = process.argv[3];
+
+if (text === 'file') {
+    fileText(path);
+} else if (text === 'url') {
+    urlText(path);
+} else {
+    console.error(`Unknown path: ${path}`);
+    process.exit(1);
+}
